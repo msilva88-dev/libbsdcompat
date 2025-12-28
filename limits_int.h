44f49726c9ca2dd2d6ae0a371747cac7ed6835af
Marcio Silva <coadde@hyperbola.info>
2025-12-27 22:14:55 -0300

    Add internal header with portable UID_MAX and GID_MAX definitions
    - Add limits_int.h for internal use,
      including <limits.h> and features.h.
    - Define GID_MAX and UID_MAX as UINT_MAX
      if not already defined by system headers.
    
    Signed-off-by: Marcio Silva <coadde@hyperbola.info>

---
+++ limits_int.h
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: CC0-1.0 */
+
+/*
+ * Public Domain
+ *
+ * Modifications to support HyperbolaBSD:
+ * Written in 2025 by Hyperbola Project
+ *
+ * To the extent possible under law, the author(s) have dedicated all copyright
+ * and related and neighboring rights to this software to the public domain
+ * worldwide. This software is distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication along
+ * with this software. If not, see
+ * <https://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+#include <limits.h>
+#include "features.h"
+
+#ifndef GID_MAX
+#define GID_MAX UINT_MAX
+#endif
+
+#ifndef UID_MAX
+#define UID_MAX UINT_MAX
+#endif
